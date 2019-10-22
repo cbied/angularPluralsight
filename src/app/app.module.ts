bb23fe7a8cf09d405a496a4254bf4247526a274e
Christopher Biediger <37456116+cbied@users.noreply.github.com>
2019-10-22 11:24:28 -0500

    finish starter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component'
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component:ProductDetailComponent },
-      { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ], { useHash: true })
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: 'welcome', component: WelcomeComponent }
+    ], { useHash: true }),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,14 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+
+
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+
+  constructor(private route: ActivatedRoute, 
+              private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id'),
+       productName = this.route.snapshot.params.productName,
+       productCode = this.route.snapshot.params.productCode,
+       releaseDate = this.route.snapshot.params.releaseDate,
+       description = this.route.snapshot.params.description,
+       price = this.route.snapshot.params.price,
+       starRating = +this.route.snapshot.params.starRating,
+       imageUrl = this.route.snapshot.params.imageUrl;
+    
+    this.pageTitle += `: ${id}`
+    this.product = {
+      'productId': id,
+      'productName': productName,
+      'productCode': productCode,
+      'releaseDate': releaseDate,
+      'description': description,
+      'price': price,
+      'starRating': starRating,
+      'imageUrl': imageUrl
+    }
+
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
